d4c0d70b0f62ad929a99d011dc812d8b9845f528
Peter Culak <peter@demobox.co>
2016-07-19 21:10:04 +0200

    Reply message fixes

--- FormType/ReplyMessageFormType.php
+++ FormType/ReplyMessageFormType.php
@@ -3,6 +3,7 @@
 namespace FOS\MessageBundle\FormType;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
@@ -16,18 +17,11 @@ class ReplyMessageFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('body', 'textarea', array('label' => 'body', 'translation_domain' => 'FOSMessageBundle'));
-    }
-
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
-    {
-        $resolver->setDefaults(array(
-            'intention'  => 'reply',
-        ));
+            ->add('body', TextareaType::class, array('label' => 'body', 'translation_domain' => 'FOSMessageBundle'));
     }
 
     public function getName()
     {
-        return 'fos_message_reply_message';
+        return '';
     }
 }
